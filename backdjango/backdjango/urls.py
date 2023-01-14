cced3e9f37c0ab8ec782c866b0c6320f124417e5
joy <sh940311@gmail.com>
2023-01-14 20:58:05 +0900

    Fix: “수정 urls.py”
    urls.py에서 DRF 테스트 코드 제거

--- backdjango/backdjango/urls.py
+++ backdjango/backdjango/urls.py
@@ -4,7 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
     path('approval/', include('approval.urls')),
     path('buy/', include('buy.urls')),
     path('order/', include('order.urls')),
