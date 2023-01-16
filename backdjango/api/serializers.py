bae365cbe342a50226c3cdea8ba2b538ddb7ed21
joy <sh940311@gmail.com>
2023-01-16 17:30:28 +0900

    Feat: “추가 api filtering을 위한 수정”
    serializer.py에 fk로 참조하는 필드 추가
    view.py에 필터링 추가

--- backdjango/api/serializers.py
+++ backdjango/api/serializers.py
@@ -1,32 +1,48 @@
 from rest_framework import serializers
 from .models import User, Reqterm, Category1, Category2, Request, Product, Order
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
 class ReqtermSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reqterm
         fields = '__all__'
+
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
         fields = '__all__'
+
+
 class RequestSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='usernum.username')
+    prodname = serializers.ReadOnlyField(source='prodnum.prodname')
+    prodprice = serializers.ReadOnlyField(source='prodnum.prodprice')
+
     class Meta:
         model = Request
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['reqnum','prodnum','prodname', 'prodprice','reqcount','reqprice','reqdate','reqapvdate','reqstate','reqstaging','reqrejectreason','reqcancled','usernum','username','termyearmonth',]
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = '__all__'
 
+
 class Category2Serializer(serializers.ModelSerializer):
     class Meta:
         model = Category2
         fields = '__all__'
+
+
 class Category1Serializer(serializers.ModelSerializer):
     class Meta:
         model = Category1
--- backdjango/api/views.py
+++ backdjango/api/views.py
@@ -1,4 +1,6 @@
 # from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.viewsets import ModelViewSet
 from .models import User, Reqterm, Request, Product, Category2, Category1, Order
 from .serializers import UserSerializer, ReqtermSerializer, RequestSerializer, ProductSerializer, Category2Serializer, \
@@ -19,6 +21,16 @@ class ReqtermViewSet(ModelViewSet):
 class RequestViewSet(ModelViewSet):
     queryset = Request.objects.all()
     serializer_class = RequestSerializer
+
+    filter_backends = [
+        DjangoFilterBackend,
+        OrderingFilter,
+        SearchFilter,
+    ]
+
+    filterset_fields = ["reqnum"]
+    ordering_fields = ["usernum", "reqdate", "reqnum"]
+    search_fields = ['termyearmonth__termyearmonth', 'usernum__username', 'reqstate', 'prodnum__prodname']
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
