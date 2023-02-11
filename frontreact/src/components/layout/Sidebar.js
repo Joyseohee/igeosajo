044ccf3694bec91ab8406d16c85dcb94811c5f98
joy <sh940311@gmail.com>
2023-02-12 02:36:08 +0900

    Feat: "추가 사이드바 토글 기능"
    사이드바 토글 가능
    클릭된 사이드바에 색 변경 필요

--- frontreact/src/components/layout/Sidebar.js
+++ frontreact/src/components/layout/Sidebar.js
@@ -1,32 +1,34 @@
 import React, {Component} from "react";
 import "../../styled/Layouts.css";
-import {Link} from "react-router-dom";
+import {Link, withRouter} from "react-router-dom";
 import Menu from "../../storage/Menu";
-
 
 class Sidebar extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            menus: [{
-                index: "depth1",
-                name: "",
-                path: "",
-            }],
-        }
+            menus: [
+                {
+                    index: "depth1",
+                    name: "",
+                    path: "",
+                },
+            ],
+            currentMenu: null,
+        };
         this.setMenu = this.setMenu.bind(this);
     }
 
-    setMenu(userathority) {
-        if (userathority === 0) {
+    setMenu(userAuthority) {
+        if (userAuthority === 0) {
             this.setState({
                 menus: new Menu().user0menu,
             });
-        } else if (userathority === 1) {
+        } else if (userAuthority === 1) {
             this.setState({
                 menus: new Menu().user1menu,
             });
-        } else if (userathority === 2) {
+        } else if (userAuthority === 2) {
             this.setState({
                 menus: new Menu().user2menu,
             });
@@ -47,32 +49,48 @@ class Sidebar extends Component {
         }
     }
 
+
+    handleMainMenuClick = (index) => {
+        this.setState({currentMenu: index});
+    };
+
     render() {
-        const {menus} = this.state;
+        const {menus, currentMenu} = this.state;
         return (
             <div className="sidebar-wrapper">
                 <div className="sidebar">
                     <div className="menu-wrapper">
-                        {menus.map((menu) => {
+                        {menus.map((menu, index) => {
                             return (
                                 <div key={menu.index}>
-                                    <div className="menu1"><a href={menu.path}>{menu.name}</a></div>
-                                    {menu.menu2 && menu.menu2.map((menue) => {
-                                        return (
-                                            <div key={menue.index}>
-                                                <div className="menu2"><Link
-                                                    to={menue.path}>{menue.name}</Link></div>
-                                            </div>
-                                        )
-                                    })}
+                                    <div
+                                        className="menu1"
+                                        value={menu.index}
+                                        onClick={() => this.handleMainMenuClick(index)}
+                                    >
+                                        <Link to={menu.path}>
+                                            {menu.name}
+                                        </Link>
+                                    </div>
+                                    {index === currentMenu &&
+                                        menu.menu2 &&
+                                        menu.menu2.map((submenu) => {
+                                            return (
+                                                <div key={submenu.index}>
+                                                    <div className="menu2">
+                                                        <Link to={submenu.path}>{submenu.name}</Link>
+                                                    </div>
+                                                </div>
+                                            );
+                                        })}
                                 </div>
-                            )
+                            );
                         })}
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
-export default Sidebar;
+export default withRouter(Sidebar);
