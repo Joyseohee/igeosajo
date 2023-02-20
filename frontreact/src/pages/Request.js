1d51d27cc74a2c65f0213f512998b2368577ad6d
joy <sh940311@gmail.com>
2023-02-20 18:01:01 +0900

    Fix: "수정 CSS 수정"

--- frontreact/src/pages/Request.js
+++ frontreact/src/pages/Request.js
@@ -159,8 +159,24 @@ class Request extends Component {
                                     pageCount={pageCount}
                                 />
                             }
-                            {available === 1 &&  (requestFilter === '대기' ||requestFilter === '전체') &&
+                            {available === 1 && (requestFilter === '대기' || requestFilter === '전체') &&
                                 <RequestButtons updateState={this.updateState} checkedRequest={checkedRequest}/>
+                            }
+                            {available === 0 &&
+                                <div className="request-alert-wrapper">
+                                    <div className="request-alert-icon">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem"
+                                             fill="rgb(248, 103, 106)" className="bi bi-exclamation-diamond-fill"
+                                             viewBox="0 0 16 16">
+                                            <path
+                                                d="M9.05.435c-.58-.58-1.52-.58-2.1 0L.436 6.95c-.58.58-.58 1.519 0 2.098l6.516 6.516c.58.58 1.519.58 2.098 0l6.516-6.516c.58-.58.58-1.519 0-2.098L9.05.435zM8 4c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995A.905.905 0 0 1 8 4zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                        </svg>
+                                    </div>
+                                    <div className="request-alert-text">
+                                        <p>현재 마감된 신청기간입니다.</p>
+                                        <p>신청 승인/반려가 불가능합니다.</p>
+                                    </div>
+                                </div>
                             }
                         </div>
                         {requestList[0] !== 'requestList' &&
